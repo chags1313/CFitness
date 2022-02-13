0f6fac140d8ab03929b46eadf05ada6cd4df723e
Cole Hagen <61998370+chags1313@users.noreply.github.com>
2022-02-12 22:55:22 -0600

    Update zcfitness.py

--- zcfitness.py
+++ zcfitness.py
@@ -37,7 +37,7 @@ def main():
     df = pd.read_sql("SELECT * FROM max_data", con = conn)
     df1 = df.rename(columns={'date_submitted':'index'}).set_index('index')
 
-    st.bar_chart(df["Q2"], use_container_width=True) 
+    st.bar_chart(df1["Q2"], use_container_width=True) 
     st.bar_chart(df1["Q3"], use_container_width=True)    
 
     
