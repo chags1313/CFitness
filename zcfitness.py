86c9c29e04b8509d3b63c88a17c74260987ca00c
Cole Hagen <61998370+chags1313@users.noreply.github.com>
2022-02-20 21:07:05 -0600

    Update zcfitness.py

--- zcfitness.py
+++ zcfitness.py
@@ -90,12 +90,11 @@ def main():
     if lifts:
         lift_df = df1[df1["Lift"].str.contains(lifts)]
         #lift_df = lift_df.rename(columns={'date_submitted':'index'}).set_index('index')
-        lift_1rm = lift_df[lift_df["Lift"].str.contains('1 rep max')]
-        lift_2rm = lift_df[lift_df["Lift"].str.contains('2 rep max')]
-        lift_3rm = lift_df[lift_df["Lift"].str.contains('3 rep max')]
-        st.write(lift_3rm)
-        lift_4rm = lift_df[lift_df["Lift"].str.contains('4 rep max')]
-        lift_5rm = lift_df[lift_df["Lift"].str.contains('5 rep max')]
+        lift_1rm = lift_df[lift_df["Reps"].str.contains('1 rep max')]
+        lift_2rm = lift_df[lift_df["Reps"].str.contains('2 rep max')]
+        lift_3rm = lift_df[lift_df["Reps"].str.contains('3 rep max')]
+        lift_4rm = lift_df[lift_df["Reps"].str.contains('4 rep max')]
+        lift_5rm = lift_df[lift_df["Reps"].str.contains('5 rep max')]
 
 
         #st.line_chart(bsq1rm["2"])
