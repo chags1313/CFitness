2c15b7c330ec5525b79a416b9931e2c615f69aab
Cole Hagen <61998370+chags1313@users.noreply.github.com>
2022-02-20 20:49:57 -0600

    Update zcfitness.py

--- zcfitness.py
+++ zcfitness.py
@@ -88,7 +88,7 @@ def main():
 
 
     if lifts:
-        lift_df = df[df["Lift"].str.contains(lifts)]
+        lift_df = df1[df1["Lift"].str.contains(lifts)]
         lift_df = lift_df.rename(columns={'date_submitted':'index'}).set_index('index')
         lift_1rm = lift_df[lift_df["Lift"].str.contains('1 rep max')]
         lift_2rm = lift_df[lift_df["Lift"].str.contains('2 rep max')]
